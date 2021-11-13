f92c6844c2f4bff5446ad5d6c98a6c03870da068
Kyle <kylewilkinson14@gmail.com>
2021-11-13 13:29:42 +1100

    add to do and delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const input = document.querySelector("#new-task-description");
+  const list = document.querySelector("#list");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newToDo = document.createElement("li");
+    newToDo.innerHTML = `${ input.value } <button onclick="deleteTask(event)">x</button>`;
+    list.append(newToDo);
+    input.value = "";
+  });
+
+
 });
+
+const deleteTask = (event) => {
+  event.target.parentNode.remove();
+};
+
+
+
+
+
+
+
+
